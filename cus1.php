38284210ad7136157e35a2f6be92d16bae2fa484
ashuuuuu191919 <126796271+ashuuuuu191919@users.noreply.github.com>
2023-03-13 15:54:55 +0530

    Add files via upload

---
+++ cus1.php
@@ -0,0 +1,6 @@
+<?php  
+include 'data.php';
+$row = mysqli_fetch_assoc($result);
+include 'c1.php';
+?>
+
---
+++ data.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "bank";
+$conn = mysqli_connect( $servername, $username, $password, $database);
+if( ! $conn){
+  die("Error Occured Please Try again Later");
+}
+$sql = "SELECT * FROM `bankrecord`";
+        $result = mysqli_query($conn, $sql);
+
+        $trans = "SELECT * FROM `bankdata`";
+        $r = mysqli_query($conn, $trans);
+?>
+
